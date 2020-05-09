57fe6f7f731689e9e8c8bd209af36e180ea53547
Gustavo <gusrigor@icloud.com>
2020-05-09 12:37:27 -0300

    adicionado programa ex1

---
+++ ex1.asm
@@ -0,0 +1,20 @@
+;---------------------------------------------------
+; Programa: Soma 5
+; Autor: Gustavo G. Rigor
+; Data:09/05/2020
+;---------------------------------------------------
+G EQU 160 ;Endereço da variável G definido como 160
+U EQU 161 ;Endereço da variável U definido como 161
+S EQU 162 ;Endereço da variável S definido como 162
+T EQU 163 ;Endereço da variável T definido como 163
+A EQU 164 ;Endereço da variável A definido como 164
+V EQU 165 ;Endereço da variável V definido como 165 
+
+ORG 0
+LDA G ;acumulador A recebe conteúdo G
+ADD U ;conteúdo de A é somado ao conteúdo de U
+ADD S ;conteúdo de A é somado ao conteúdo de S
+ADD T ;conteúdo de A é somado ao conteúdo de T
+ADD A ;conteúdo de A é somado ao conteúdo de A
+STA V ;conteúdo de A é copiado para V
+HTL   ;processador para
