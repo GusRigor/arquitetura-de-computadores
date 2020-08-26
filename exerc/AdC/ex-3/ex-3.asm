c21615cccc7a7a9ed3828a613ca68cbdf3ae0ab5
Gustavo Rigor <gusrigor@icloud.com>
2020-08-26 15:02:41 -0300

    Atualizando o exerc-3, com bug no vai um

--- exerc/AdC/ex-3/ex-3.asm
+++ exerc/AdC/ex-3/ex-3.asm
@@ -11,8 +11,11 @@ M EQU 242 ;Posição F2 é M maior num
 N EQU 243 ;Posição F3 é N menor num
 
 U EQU 244 ;Posição F4 é U iterador
+F EQU 248 ;Posição F8 é F valor FF
+
 V EQU 245 ;Posição F5 é V auxiliar
-R EQU 246 ;Posição F6 é R resultado
+C EQU 246 ;Posição F6 é C centena
+R EQU 247 ;Posição F7 é R resultado
 
 ORG 0
 
@@ -40,16 +43,41 @@ COMECO:
   SUB V ; Faz a diferença entre N e V
   JN FIM ; Se menor, termina a multiplicação
   JZ FIM ; Se zero, termina a multiplicação
-   ;Começa a multiplicação
+
+   ;Verificar se "vai um"
+     LDA F ; Copia o conteúdo de R (resultado) para o acumulador A
+     NOT
+     ADD R ; Subtrai o valor FF do valor resultado
+     ADD M ; Adiciona M (maior) Acc = (R-F)+M
+     STA 129
+     JN VaiUm
+     JZ VaiUm
+
+  ;Começa a multiplicação
+   LDA M ; Copia o conteúdo de M (maior) para o acumulador A
+   ADD R ; Soma R com o Acumulador A
+   STA R ; Copia o acumulador A para R
+   LDA V ; Incrementar V em 1
+   ADD U
+   STA V
+   JMP COMECO ; Faz o loop para multiplicação
+     
+VaiUm:
+     LDA C
+     ADD U
+     STA C
      LDA M ; Copia o conteúdo de M (maior) para o acumulador A
      ADD R ; Soma R com o Acumulador A
      STA R ; Copia o acumulador A para R
      LDA V ; Incrementar V em 1
      ADD U
      STA V
-     JMP COMECO ; Faz o loop para multiplicação
+     JMP COMECO
+  
 
 FIM:
+  LDA C ; Copia o conteúdo de C para o acumulador A
+  OUT 0 ; Mostra no visor o valor do acumulador A
   LDA R ; Copia o conteúdo de R (resultado) para o acumulador A
   OUT 0 ; Mostra no visor o valor do acumulador A
   HLT ; Termina o programa
